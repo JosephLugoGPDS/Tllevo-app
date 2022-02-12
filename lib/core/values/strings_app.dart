ec3b1d5e3b83a25eff13e20aeb70492387eaa3b4
Joseph Lugo <josephfronted1@gmail.com>
2022-02-12 18:59:57 -0500

    chore: create strings app

---
+++ lib/core/values/strings_app.dart
@@ -0,0 +1,17 @@
+class StringsApp {
+  //toDo: implements l10n
+  static String home() => 'Inicio';
+  static String search() => 'Buscar';
+  static String myShoppings() => 'Mis Compras';
+  static String favorites() => 'Favoritos';
+  static String discounts() => 'Descuentos';
+  static String history() => 'Historial';
+  static String myAccount() => 'Mi cuenta';
+  static String sell() => 'Vender';
+  static String officialStore() => 'Tiendas Oficiales';
+  static String categories() => 'Categorías';
+  static String help() => 'Ayuda';
+  static String about() => 'Acerca De';
+  static String youWantToAccessTheBestDiscount() => '¿Deseas acceder a los mejores descuentos?';
+  static String signIn() => 'Iniciar Sesión De';
+}
