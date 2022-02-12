47d11069f7b9387f2b50de3a2d4c28493c6c9837
Joseph Lugo <josephfronted1@gmail.com>
2022-02-12 18:33:45 -0500

    chore: add dimmensions in app

---
+++ lib/core/values/dimension.dart
@@ -0,0 +1,13 @@
+const double MARGIN_SIZE_DEFAULT = 18;
+const double MARGIN_SIZE_MEDIUM = 20;
+const double MARGIN_SIZE_LARGE = 22;
+const double MARGIN_SIZE_XXLL = 60;
+
+const double PADDING_SIZE_DEFAULT = 15;
+const double PADDING_SIZE_MEDIUM = 20;
+const double PADDING_SIZE_LARGE = 22;
+const double PADDING_SIZE_XXLL = 60;
+
+const double ICON_SIZE_DEFAULT = 30;
+const double ICON_SIZE_SMALL = 25;
+
