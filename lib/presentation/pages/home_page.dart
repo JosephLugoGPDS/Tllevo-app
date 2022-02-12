d6142a1d1253c4e27c475d707f98378e0b47931c
Joseph Lugo <josephfronted1@gmail.com>
2022-02-12 18:45:25 -0500

    chore: add app bar in home page

---
+++ lib/presentation/pages/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:tllevo/core/theme/colors_app.dart';
+import 'package:tllevo/core/values/dimension.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: primaryColor,
+        elevation: 0,
+        backwardsCompatibility: false,
+        systemOverlayStyle: SystemUiOverlayStyle(statusBarColor: primaryColor),
+        title: Image.asset('assets/brand.png'),
+        actions: [
+          IconButton(onPressed: () => null, icon: const Icon(Icons.notifications_sharp, size: ICON_SIZE_DEFAULT)),
+          IconButton(onPressed: () => null, icon: const Icon(Icons.favorite, size: ICON_SIZE_DEFAULT))
+        ],
+      ),
+      body: Container(
+        height: double.maxFinite
+      ),
+    );
+  }
+}
