7e0c5f0c2c3e5dd8670dd1b325ce655aa0d3bd59
Joseph Lugo <josephfronted1@gmail.com>
2022-02-12 17:59:28 -0500

    config: change name app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:tllevo/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(const TllevoApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
