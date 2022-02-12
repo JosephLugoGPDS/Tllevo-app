95b193ba8a3231afa42d1ef9c7b68fa5ff83b99e
Joseph Lugo <josephfronted1@gmail.com>
2022-02-12 18:10:33 -0500

    chore: add colors app

---
+++ lib/core/theme/colors_app.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+const Color primaryColor = Color(0xFF4C2EAC);
+const Color backgroundColor = Color(0xFFEEEEEE);
+const Color whiteColor = Color(0xFFFFFFFF);
+const Color subTextColor = Color(0xFF2A2A2A);
+const Color textColor = Color(0xFF2D2D2D);
+const Color blueColor = Color(0xFF3483FA);
+const Color buttonColor = Color(0xFF642EAC);
+const Color greyColor = Color(0xFF888888);
+const Color greyLightColor = Color(0xFFA0A5A9);
+const Color greydarkColor = Color(0xFF707070);
