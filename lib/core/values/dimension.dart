ccb51a17e5052f5ed67e0223a67518548c7a6221
Joseph Lugo <josephfronted1@gmail.com>
2022-02-12 23:01:54 -0500

    chore: add others dimmensions app

--- lib/core/values/dimension.dart
+++ lib/core/values/dimension.dart
@@ -2,7 +2,7 @@ const double MARGIN_SIZE_EXTRA_SMALL = 6;
 const double MARGIN_SIZE_SMALL = 12;
 const double MARGIN_SIZE_DEFAULT = 18;
 const double MARGIN_SIZE_MEDIUM = 20;
-const double MARGIN_SIZE_LARGE = 22;
+const double MARGIN_SIZE_LARGE = 25;
 const double MARGIN_SIZE_XXLL = 60;
 
 const double PADDING_SIZE_SMALL = 10;
@@ -15,6 +15,12 @@ const double ICON_SIZE_DEFAULT = 30;
 const double ICON_SIZE_MEDIUM = 40;
 const double ICON_SIZE_LARGE = 50;
 const double ICON_SIZE_SMALL = 25;
+const double ICON_SIZE_EXTRA_SMALL = 16;
 
 const double RADIUS_SIZE_SMALL = 5;
+const double RADIUS_SIZE_MEDIUM = 10;
+const double RADIUS_SIZE_MAX_MEDIUM = 30;
+const double RADIUS_SIZE_DEFAULT = 52;
+const double RADIUS_SIZE_LARGE = 60;
+const double RADIUS_SIZE_XLARGE = 70;
 
