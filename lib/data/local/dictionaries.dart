1df475487fa26bb6270b46b5f64c6597ce5ac1ec
Joseph Lugo <josephfronted1@gmail.com>
2022-02-12 20:14:07 -0500

    fix: drawer map

--- lib/data/local/dictionaries.dart
+++ lib/data/local/dictionaries.dart
@@ -17,34 +17,38 @@ Map<int, ShrinkDrawerItem> drawerMap = {
     StringsApp.myShoppings(),
   ),
   3: ShrinkDrawerItem(
+    Icons.favorite_outline,
+    StringsApp.favorites(),
+  ),
+  4: ShrinkDrawerItem(
     Icons.bar_chart_outlined,
     StringsApp.discounts(),
   ),
-  4: ShrinkDrawerItem(
+  5: ShrinkDrawerItem(
     Icons.history,
     StringsApp.history(),
   ),
-  5: ShrinkDrawerItem(
+  6: ShrinkDrawerItem(
     Icons.person_outline,
     StringsApp.myAccount(),
   ),
-  6: ShrinkDrawerItem(
+  7: ShrinkDrawerItem(
     Icons.sell_outlined,
     StringsApp.sell(),
   ),
-  7: ShrinkDrawerItem(
+  8: ShrinkDrawerItem(
     Icons.store_outlined,
     StringsApp.officialStore(),
   ),
-  8: ShrinkDrawerItem(
+  9: ShrinkDrawerItem(
     Icons.category_outlined,
     StringsApp.categories(),
   ),
-  9: ShrinkDrawerItem(
+  10: ShrinkDrawerItem(
     Icons.help_outline_outlined,
     StringsApp.help(),
   ),
-  10: ShrinkDrawerItem(
+  11: ShrinkDrawerItem(
     Icons.brightness_auto_outlined,
     StringsApp.about(),
   ),
