ccc24fcd3df4c0257237ce8a65e318176cb474c4
Joseph Lugo <josephfronted1@gmail.com>
2022-02-12 23:01:27 -0500

    chore: add others strings app

--- lib/core/values/strings_app.dart
+++ lib/core/values/strings_app.dart
@@ -13,5 +13,14 @@ class StringsApp {
   static String help() => 'Ayuda';
   static String about() => 'Acerca De';
   static String youWantToAccessTheBestDiscount() => '¿Deseas acceder a los mejores descuentos?';
-  static String signIn() => 'Iniciar Sesión De';
+  static String signIn() => 'Iniciar Sesión';
+  static String signUp() => 'Crea tu cuenta';
+  static String searchTheProductyouWant() => 'Busca el producto que deseas';
+  static String allCategories() => 'Todas Categorías';
+  static String videoPlayer() => 'Videojuegos';
+  static String moda() => 'Moda';
+  static String computing() => 'Computación';
+  static String audio() => 'Audio';
+  static String tv() => 'Televisores';
+  static String enjoyTheExperienceBetter() => '¡Disfruta mejor la experiencia y crea tu cuenta Tllevo!';
 }
