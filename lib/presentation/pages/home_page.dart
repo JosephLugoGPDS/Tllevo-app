1586f52d23a3871090328fec264161ce77ab6ea9
Joseph Lugo <josephfronted1@gmail.com>
2022-02-12 20:50:22 -0500

    chore: add primary offerts cards in home page

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:tllevo/core/theme/colors_app.dart';
 import 'package:tllevo/core/values/dimension.dart';
 import 'package:tllevo/core/values/strings_app.dart';
+import 'package:tllevo/presentation/pages/widgets/primary_offerts_cards.dart';
 import 'package:tllevo/presentation/widgets/drawer/shrinkdrawer.dart';
 import 'package:tllevo/presentation/widgets/search_field.dart';
 
@@ -33,9 +34,10 @@ class HomePage extends StatelessWidget {
           ),
           ListView(
             children: [
-              SearchField(StringsApp.searchTheProductyouWant())
+              SearchField(StringsApp.searchTheProductyouWant()),
+              const PrimaryOffertsCards()
             ],
-          )
+          ),
         ],
       ),
     );
